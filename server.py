cf4bc08b7a9d73febff6c0e88bbebae46907b877
Avneesh-Sinha <avneesh0904@gmail.com>
2024-10-18 02:14:27 +0530

    port change

--- server.py
+++ server.py
@@ -44,4 +44,4 @@ def pull_changes():
     return jsonify({'filename': filename, 'content': content}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8888)
